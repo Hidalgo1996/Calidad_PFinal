9587490004dad53e0e8a6ffe5bf2a04a8a176431
Hidalgo1996 <GeForce210_1996@hotmail.com>
2023-06-11 17:48:41 -0500

    ligeras-correciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,18 +11,18 @@ import { FooterComponent } from './Footer/Footer.component';
 import { CabeceraComponent } from './Cabecera/Cabecera.component';
 import { Pagina_inicialComponent } from './Pagina_inicial/Pagina_inicial.component';
 
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
-import {MatTableModule} from '@angular/material/table';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatTabsModule} from '@angular/material/tabs';
-import { ReactiveFormsModule,FormsModule } from '@angular/forms';
+import { MatButtonModule}  from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { SharedService } from './Service/shared.service';
 import { Busqueda_dniComponent } from './Busqueda_dni/Busqueda_dni.component';
 import { Horario_estudianteComponent } from './Horario_estudiante/Horario_estudiante.component';
@@ -52,7 +52,6 @@ import { Perfil_docenteComponent } from './Perfil_docente/Perfil_docente.compone
       Horario_docenteComponent,
       Perfil_docenteComponent
    ],
-
   imports: [
     BrowserModule,
     AppRoutingModule,
