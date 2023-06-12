b74025a4feef1187f9aa1ca7836b373de55fe44f
DiegoEli <diego.mendozam@ug.edu.ec>
2023-06-12 05:30:00 -0500

    se modifico la navegacion de pagina docente y pagina estudiante

--- src/app/Pagina_docente/Pagina_docente.component.ts
+++ src/app/Pagina_docente/Pagina_docente.component.ts
@@ -1,34 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-Pagina_docente',
   templateUrl: './Pagina_docente.component.html',
   styleUrls: ['./Pagina_docente.component.css']
 })
 export class Pagina_docenteComponent implements OnInit {
+  mostrarHorario = false;              // Agrega la variable mostrar aquí
+  mostrarSalario = false;              // Agrega la variable mostrar aquí
+  mostrarPerfil = false;               // Agrega la variable mostrar aquí
 
   constructor(private router: Router) { }
-
 
   ngOnInit() {
   }
 
-
   ver_horario_docente(){
-
-    this.router.navigate(['/Horario_docente']);//Me envia a la ruta horario_docente
+    this.mostrarHorario = true;
+    this.mostrarSalario = false;
+    this.mostrarPerfil = false;
   }
 
   ver_salario(){
-
-    this.router.navigate(['/Remuneracion_docente']);//Me envia a la ruta remuneracion_docente
+    this.mostrarSalario = true;
+    this. mostrarHorario = false;
+    this.mostrarPerfil = false;
   }
 
   ver_perfil_docente(){
-
-    this.router.navigate(['/Perfil_docente']);//Me envia a la ruta perfil_docente
+    this.mostrarPerfil = true;
+    this.mostrarHorario = false;
+    this.mostrarSalario = false;
   }
+
+  //INTERFAZ MODIFICADA-REVISAR
+  // ver_horario_docente(){
+  //   this.router.navigate(['/Horario_docente']);//Me envia a la ruta horario_docente
+  // }
+
+  // ver_salario(){
+  //   this.router.navigate(['/Remuneracion_docente']);//Me envia a la ruta remuneracion_docente
+  // }
+
+  // ver_perfil_docente(){
+  //   this.router.navigate(['/Perfil_docente']);//Me envia a la ruta perfil_docente
+  // }
 
 }
--- src/app/Pagina_estudiante/Pagina_estudiante.component.ts
+++ src/app/Pagina_estudiante/Pagina_estudiante.component.ts
@@ -1,34 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-Pagina_estudiante',
   templateUrl: './Pagina_estudiante.component.html',
   styleUrls: ['./Pagina_estudiante.component.css']
 })
 export class Pagina_estudianteComponent implements OnInit {
+  mostrarHorario = false;              // Agrega la variable mostrar aquí
+  mostrarCalificaciones = false;       // Agrega la variable mostrar aquí
+  mostrarPerfil = false;               // Agrega la variable mostrar aquí
 
   constructor(private router: Router) { }
-
 
   ngOnInit() {
   }
 
-
   ver_horario_estudiante(){
-
-    this.router.navigate(['/Horario_estudiante']);//Me envia a la ruta horario_estudiante
+    this.mostrarHorario = true;
+    this.mostrarCalificaciones = false;
+    this.mostrarPerfil = false;
   }
 
   ver_calificaciones_estudiante(){
-
-    this.router.navigate(['/Calificaciones_estudiante']);//Me envia a la ruta calificaciones_estudiante
+    this.mostrarCalificaciones = true;
+    this. mostrarHorario = false;
+    this.mostrarPerfil = false;
   }
 
   ver_perfil_estudiante(){
-
-    this.router.navigate(['/Perfil_estudiante']);//Me envia a la ruta perfil_estudiante
+    this.mostrarPerfil = true;
+    this.mostrarHorario = false;
+    this.mostrarCalificaciones = false;
   }
+
+  //INTERFAZ MODIFICADA-REVISAR
+  // ver_horario_estudiante(){
+  //   this.router.navigate(['/Horario_estudiante']);//Me envia a la ruta horario_estudiante
+  // }
+
+  // ver_calificaciones_estudiante(){
+  //   this.router.navigate(['/Calificaciones_estudiante']);//Me envia a la ruta calificaciones_estudiante
+  // }
+
+  // ver_perfil_estudiante(){
+  //   this.router.navigate(['/Perfil_estudiante']);//Me envia a la ruta perfil_estudiante
+  // }
+
 
 }
